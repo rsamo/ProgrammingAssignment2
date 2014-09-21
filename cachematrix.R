643ec4470675fbd9a164cbf5698ed17b5a80116b
rsamo <one70six@gmail.com>
2014-09-21 13:36:31 -0500

    Revert "Initial commit of ProgrammingAssignment2 fork. Functions tested and working according to instructions"
    This reverts commit 43f42ba8f67b0e1883ecd7760026b7268a3cb853.

--- cachematrix.R
+++ cachematrix.R
@@ -1,57 +1,15 @@
-#================================================================
-# File:         cachematrix.R   
-# Name:         Programming Assignment 2: Caching the Inverse of 
-#               a Matrix
-# Desc:         This file contains functions that both take the 
-#               inverse of a matrix and cache its value for 
-#               faster access
-# Auth:         Ryan Samo
-# Date:         9-15-2014
-# Test:         Call like...
-#               m <- matrix(data = 1:4, nrow = 2, ncol = 2)
-#               z <- makeCacheMatrix(m)
-#               cacheSolve(z)
-#================================================================
+## Put comments here that give an overall description of what your
+## functions do
 
-#================================================================
-# Func:         makeCacheMatrix   
-# Desc:         Returns a matrix if already cached otherwise it
-#               caches and returns the matrix
-# Auth:         Ryan Samo
-# Date:         9-15-2014
-#================================================================
-makeCacheMatrix <- function(inputMatrix = matrix()) {
-        
-        inputCache <- NULL
-        set <- function(setMatrix) {
-                inputMatrix <<- setMatrix
-                inputCache <<- NULL
-        }
-        get <- function() inputMatrix
-        setCache <- function(solve) inputCache <<- solve
-        getCache <- function() inputCache
-        list(set = set, get = get,
-             setCache = setCache,
-             getCache = getCache)
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
-#================================================================
-# Func:         cacheSolve   
-# Desc:         Returns the inverse of a given matrix
-# Auth:         Ryan Samo
-# Date:         9-15-2014
-#================================================================
-cacheSolve <- function(inputMatrix, ...) {
-        
-        inputCache <- inputMatrix$getCache()
-        
-        if(!is.null(inputCache)) {
-                print("Getting cached data")
-                return(inputCache)
-        }
-        print("Getting non-cached data")
-        data <- inputMatrix$get()
-        inputCache <- solve(data)
-        inputMatrix$setCache(inputCache)
-        inputCache
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
